66cdc3a12448a7897513f5a1557d320ca31085c6
MHLoyche <MHLoyche@outlook.com>
2026-01-19 12:26:33 +0100

    Added launcher icon

--- app/src/main/java/com/example/nicotinetracker/MainActivity.kt
+++ app/src/main/java/com/example/nicotinetracker/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.nicotinetracker.ui.theme.NicotineTrackerTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
